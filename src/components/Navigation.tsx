57cd90e2a8b7b113a882c863c9d0132d383948af
KaiWritesCode <kbechdel8@gmail.com>
2022-05-17 14:46:45 -0400

    Finsished sidebar component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,33 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   GlobeAltIcon,
   ChatAltIcon,
   BellIcon,
   AdjustmentsIcon,
   MoonIcon,
+  MenuIcon
 } from "@heroicons/react/outline";
 
-export default function Navigation() {
+export default function Navigation({ isOpen, openModal }: any) {
+
+
   return (
     <nav>
       <div className="flex items-center justify-between border px-6 py-2">
+        <MenuIcon
+          onClick={openModal} className={`${`w-6 xl:hidden block cursor-pointer absolute
+          `} ${isOpen ? "hidden" : "block"}`} />
+
         <section>
-          <div className="flex items-center justify-center">
+          <div className="ml-8 xl:ml-0 flex items-center justify-center w-full">
             <div className="flex border-2 rounded">
               <input
                 type="text"
-                className="px-4 py-2 w-24 sm:w-52"
+                className="px-4 py-1.5 w-24 sm:w-40"
                 placeholder="Search..."
               />
-              <button className="flex items-center justify-center px-4 border-l">
+              <button className="flex items-center justify-center px-2 border-l">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-gray-700"
+                  className="h-5 w-4 text-gray-700"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                   strokeWidth={2}
-
                 >
                   <path
                     strokeLinecap="round"
@@ -40,22 +46,24 @@ export default function Navigation() {
           </div>
         </section>
         <section className="flex flex-center items-center">
-          <div className="flex flex-center items-center text-gray-700 cursor-pointer">
+          <div className="flex-center items-center hidden sm:flex text-gray-700 cursor-pointer first-letter:">
             <GlobeAltIcon width="25" className="mr-1" />
             English
           </div>
-          <div className="ml-5 text-gray-700 cursor-pointer">
+          <div className="ml-5 text-gray-700 hidden sm:block cursor-pointer">
             <MoonIcon width="25" />
           </div>
           <div className="ml-4 text-gray-700 relative  cursor-pointer">
             <BellIcon width="25" />
-            <div className="w-4 h-4 bg-red-600 text-white rounded-full flex justify-center items-center text-sm absolute -top-2 -right-1">1</div>
+            <div className="w-4 h-4 bg-red-600 text-white rounded-full flex justify-center
+             items-center text-sm absolute -top-2 -right-1 weight">1</div>
           </div>
-          <div className="ml-4 text-gray-700 relative cursor-pointer">
-            <div className="w-4 h-4 bg-red-600 text-white rounded-full flex justify-center items-center text-sm absolute -top-2 -right-1 ">2</div>
+          <div className="ml-4 text-gray-700 relative cursor-pointer hidden sm:block">
+            <div className="w-4 h-4 bg-red-600 text-white rounded-full flex justify-center items-center
+             text-sm absolute -top-2 -right-1 ">2</div>
             <ChatAltIcon width="25" />
           </div>
-          <div className="ml-4 text-gray-700 cursor-pointer">
+          <div className="ml-4 text-gray-700 cursor-pointer hidden sm:block">
             <AdjustmentsIcon width="25" />
           </div>
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../App.css"
 import Navigation from "../components/Navigation";
+import Sidebar from "../components/Sidebar";
 
 function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openModal = () => {
+    setIsOpen(true)
+    console.log(isOpen)
+  }
+
+  useEffect(() => {
+    openModal()
+  }, [setIsOpen])
+
   return (
-    <div>
-      <Navigation />
+    <div className="flex w-full">
+      <Sidebar />
+      <div className="w-full" onClick={() => setIsOpen(false)}>
+        <Navigation openModal={openModal} isOpen={isOpen} />
+      </div>
+
     </div>
   );
 }
